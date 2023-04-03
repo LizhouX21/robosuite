b85e188548edfc63a3d8a96fcd881c17842aba44
BadbotX <lizhou.xu@outlook.com>
2023-04-03 14:58:11 +0200

    Lift vecenv early termination monitor

--- robosuite/environments/manipulation/lift.py
+++ robosuite/environments/manipulation/lift.py
@@ -164,6 +164,7 @@ class Lift(SingleArmEnv):
         camera_segmentations=None,  # {None, instance, class, element}
         renderer="mujoco",
         renderer_config=None,
+        early_terminations=True
     ):
         # settings for table top
         self.table_full_size = table_full_size
@@ -179,6 +180,8 @@ class Lift(SingleArmEnv):
 
         # object placement initializer
         self.placement_initializer = placement_initializer
+
+        self.early_terminations=early_terminations
 
         super().__init__(
             robots=robots,
@@ -237,6 +240,8 @@ class Lift(SingleArmEnv):
         # sparse completion reward
         if self._check_success():
             reward = 2.25
+        elif self._below_table():
+            reward=-2.25
 
         # use a shaping reward
         elif self.reward_shaping:
@@ -255,14 +260,47 @@ class Lift(SingleArmEnv):
         # Scale reward if requested
         if self.reward_scale is not None:
             reward *= self.reward_scale / 2.25
-
+        #print(reward)
         return reward
+    
+
+
+
+        # my rewards
+        # reward = 0.0
+
+        # sparse completion reward
+        # if self._check_success():
+        #     reward = 2.25
+
+        # use a shaping reward
+        # elif self.reward_shaping:
+
+        #     reaching reward
+        #     cube_pos = self.sim.data.body_xpos[self.cube_body_id]
+        #     gripper_site_pos = self.sim.data.site_xpos[self.robots[0].eef_site_id]
+        #     dist = np.linalg.norm(gripper_site_pos - cube_pos)
+        #     reaching_reward = 1 - np.tanh(10.0 * dist)
+        #     reward += reaching_reward
+
+        #     grasping reward
+        #     if self._check_grasp(gripper=self.robots[0].gripper, object_geoms=self.cube):
+        #         reward += 0.25
+
+        # Scale reward if requested
+        # if self.reward_scale is not None:
+        #     reward *= self.reward_scale / 2.25
+        # print(reward)
+        # return reward
+        # my rewards
+
+
 
     def _load_model(self):
         """
         Loads an xml model, puts it in self.model
         """
-        print("episode begin")
+        #print("episode begin")
         super()._load_model()
 
         # Adjust base pose accordingly
@@ -362,15 +400,15 @@ class Lift(SingleArmEnv):
             def cube_quat(obs_cache):
                 return convert_quat(np.array(self.sim.data.body_xquat[self.cube_body_id]), to="xyzw")
 
-            @sensor(modality=modality)
-            def gripper_to_cube_pos(obs_cache):
-                return (
-                    obs_cache[f"{pf}eef_pos"] - obs_cache["cube_pos"]
-                    if f"{pf}eef_pos" in obs_cache and "cube_pos" in obs_cache
-                    else np.zeros(3)
-                )
+            # @sensor(modality=modality)
+            # def gripper_to_cube_pos(obs_cache):
+            #     return (
+            #         obs_cache[f"{pf}eef_pos"] - obs_cache["cube_pos"]
+            #         if f"{pf}eef_pos" in obs_cache and "cube_pos" in obs_cache
+            #         else np.zeros(3)
+            #     )
 
-            sensors = [cube_pos, cube_quat, gripper_to_cube_pos]
+            sensors = [cube_pos, cube_quat]
             names = [s.__name__ for s in sensors]
 
             # Create observables
@@ -427,3 +465,63 @@ class Lift(SingleArmEnv):
 
         # cube is higher than the table top above a margin
         return cube_height > table_height + 0.04
+
+    def _below_table(self):
+        if self._eef_xpos[2]<=self.table_offset[2]:
+            #print("Below Table!Early Stop!")
+            return True
+        else:
+            return False
+        
+    def _post_action(self, action):
+        """
+        In addition to super method, add additional info if requested
+        Args:
+            action (np.array): Action to execute within the environment
+        Returns:
+            3-tuple:
+                - (float) reward from the environment
+                - (bool) whether the current episode is completed or not
+                - (dict) info about current env step
+        """
+        reward, done, info = super()._post_action(action)
+
+        # allow episode to finish early if allowed
+        if self.early_terminations:
+            done = done or self._check_terminated()
+
+        return reward, done, info
+
+    def _check_terminated(self):
+        """
+        Check if the task has completed one way or another. The following conditions lead to termination:
+            - Collision
+            - Task completion (insertion succeeded)
+            - Joint Limit reached
+        Returns:
+            bool: True if episode is terminated
+        """
+
+        terminated = False
+
+        # # Prematurely terminate if contacting the table with the arm
+        # if self.check_contact(self.robots[0].robot_model):
+        #     #if self.print_results:
+        #         #print(40 * "-" + " COLLIDED " + 40 * "-")
+        #     terminated = True
+
+        # # Prematurely terminate if task is success
+        # if self._check_success():
+        #     if self.print_results:
+        #         print(40 * "+" + " FINISHED WIPING " + 40 * "+")
+        #     terminated = True
+
+        # Prematurely terminate if over q limit
+        if self.robots[0].check_q_limits():
+            terminated = True
+
+        # Prematurely terminate below table
+        if self._below_table():
+            terminated = True
+
+        return terminated
